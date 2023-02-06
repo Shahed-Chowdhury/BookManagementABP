0b798e52ae10d0d0e00073347bafdda1ed0c194e
Shahed-Chowdhury <cslintern.shahed02@gmail.com>
2023-02-06 19:39:53 +0600

    fix: frontend

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { BookService } from './../proxy/books/book.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./book.component.scss']
 })
 export class BookComponent {
+  constructor(private bookService: BookService){}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 }
