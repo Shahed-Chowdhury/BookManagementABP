5df9cb5ddc7bc1a16759ac905b91d1a81436511f
Shahed-Chowdhury <cslintern.shahed02@gmail.com>
2023-02-09 14:56:41 +0600

    crud author done frontend

--- angular/src/app/author/author.component.ts
+++ angular/src/app/author/author.component.ts
@@ -3,14 +3,16 @@ import { ListService, PagedResultDto } from '@abp/ng.core';
 import { AuthorService } from './../proxy/authors/author.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss'],
-  providers: [ListService]
+  providers: [ListService, { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter } ]
 })
+
 export class AuthorComponent implements OnInit {
   
   author= {items: [], totalCount: 0} as PagedResultDto<AuthorDTO>
@@ -18,7 +20,10 @@ export class AuthorComponent implements OnInit {
   selectedAuthor = {} as AuthorDTO
   form: FormGroup
 
-  constructor(public readonly list: ListService, private fb: FormBuilder, private authorService: AuthorService) {}
+  constructor(public readonly list: ListService,
+    private fb: FormBuilder,
+    private authorService: AuthorService,
+    private confirmation: ConfirmationService) {}
   
   ngOnInit(): void {
       const authorStreamCreator = query => this.authorService.getList(query)
@@ -38,21 +43,45 @@ export class AuthorComponent implements OnInit {
 
   deleteAuthor(authorId: string)
   {
-    alert(authorId)
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe((status) => {
+      if (status === Confirmation.Status.confirm) {
+        this.authorService.delete(authorId).subscribe(() => this.list.get());
+      }
+    });
   }
 
   editAuthor(authorId: string)
   {
-    alert(authorId)
+    this.authorService.get(authorId).subscribe((author)=>{
+      this.selectedAuthor = author
+      this.buildForm();
+      this.isModalOpen = true
+    })
   }
 
   buildForm(){
     this.form = this.fb.group({
       name: [this.selectedAuthor.name || '', Validators.required],
       shortBio: [this.selectedAuthor.shortBio || '', Validators.required],
-      dob: [this.selectedAuthor.birthDate ? new Date(this.selectedAuthor.birthDate) : null, Validators.required]
+      birthDate: [this.selectedAuthor.birthDate ? new Date(this.selectedAuthor.birthDate) : null, Validators.required]
     })
   }
 
-  save(){alert("Ok")}
+  save(){
+    if(this.form.invalid){
+      return;
+    }
+
+    const request = this.selectedAuthor.id
+    ? this.authorService.update(this.selectedAuthor.id, this.form.value)
+    : this.authorService.create(this.form.value);
+
+    // console.log(this.form.value);
+
+    request.subscribe(()=>{
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
+    })
+  }
 }
